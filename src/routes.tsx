f64738527dc091e26154304a5f420a7b84e246a0
Jorge Dias <jorgedsdias@gmail.com>
2020-06-14 22:15:34 -0300

    removendo teste

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,6 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePoint from './pages/CreatePoint';
 import FilteredPoints from './pages/FilteredPoints';
-import Teste from './pages/Teste';
 
 const Routes = () => {
     return (
@@ -12,7 +11,6 @@ const Routes = () => {
             <Route component={Home} path="/" exact />
             <Route component={CreatePoint} path="/create-point" />
             <Route component={FilteredPoints} path="/filtered-points" />
-            <Route component={Teste} path="/teste" />
         </BrowserRouter>
     );
 }
